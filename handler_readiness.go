0b6bd46bbb18ed05acc4a6f8bceb83875fcc1cb6
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-05-29 22:20:01 +0000

    fix: Add readiness handler response with JSON

--- handler_readiness.go
+++ handler_readiness.go
@@ -5,5 +5,5 @@ import "net/http"
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	// This handler is used to check if the service is ready
 	// It can be used by Kubernetes or other orchestrators to determine service readiness
-	
+	respondWithJSON(w, 200, struct{}{})
 }
